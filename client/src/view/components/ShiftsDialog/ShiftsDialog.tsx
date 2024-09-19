586832ebb445a537fa2b228c49402b6f85390af7
Italo Covas <italocovas@gmail.com>
2024-09-18 22:14:34 -0300

    feat: message for empty shifts

--- client/src/view/components/ShiftsDialog/ShiftsDialog.tsx
+++ client/src/view/components/ShiftsDialog/ShiftsDialog.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { ImSad } from "react-icons/im";
 import { Shift } from "../../../shared/types/health-unit.type";
 import { HealthUnit } from "../HealthUnit";
 import { formatDate } from "../../../shared/utils/formatDate";
@@ -36,12 +37,24 @@ export function ShiftsDialog({ name, shifts, id }: ShiftsDialogProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="bg-gray-100/50 data-[state=open]:animate-overlayShow fixed inset-0" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-          <Dialog.Title className="m-0 text-[17px] text-center font-medium">
-            Aplicar a um turno
-          </Dialog.Title>
-          <Dialog.Description className="mt-[10px] mb-5 text-[15px] leading-normal text-center">
-            Faça sua aplicação no turno desejado.
-          </Dialog.Description>
+          {shifts.length > 0 ? (
+            <>
+              <Dialog.Title className="m-0 text-[17px] text-center font-medium">
+                Aplicar a um turno
+              </Dialog.Title>
+              <Dialog.Description className="mt-[10px] mb-5 text-[15px] leading-normal text-center">
+                Faça sua aplicação no turno desejado.
+              </Dialog.Description>
+            </>
+          ) : (
+            <div className="flex flex-col items-center">
+              <Dialog.Title />
+              <Dialog.Description className="mt-[10px] mb-5 text-[15px] leading-normal text-center font-bold">
+                Não há turnos disponíveis para esta unidade.
+              </Dialog.Description>
+              <ImSad className="text-[#4062F9] w-8 h-8" />
+            </div>
+          )}
           <div className="flex flex-col gap-y-4">
             {shifts.map((shift) => {
               const formattedStartDatetime = formatDate(shift.startDatetime);
